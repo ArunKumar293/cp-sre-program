66e9e15be063dfe3e56d81d3b08b5bc59550165b
ArunKumar293 <arunkumar2932000@gmail.com>
2020-05-21 00:42:15 +0530

    DP

--- slidingWindowSum.py
+++ slidingWindowSum.py
@@ -1,8 +1,8 @@
 N,K=map(int,input().split())
 arr=list(map(int,input().split()))
-maxSum=sum(arr[:K])
-for index in range(1,N-K+1):
-    currSum=sum(arr[index:index+K])
-    if currSum>maxSum:
-        maxSum=currSum
+windowSum=sum(arr[:K])
+maxSum=windowSum
+for index in range(N-K):
+    windowSum=windowSum-arr[index]+arr[index+K]
+    maxSum=max(maxSum,windowSum)
 print(maxSum)
